356fdf262154219e738d7469d69c64cc11a0e268
Ben Green <bengreen5@yahoo.com>
2026-09-05 19:51:40 +0100

    Correct some errors and correct ordering.

--- chroot_umount.sh
+++ chroot_umount.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 set -x
 set -e
-BASELOC=$1
-if [ ! -d "$BASELOC" ] ;
+BASELOC="${1}"
+if [ -z "${BASELOC}" ] || [ ! -d "${BASELOC}" ] ; then
 	echo "No base location specified" 1>&2
 	exit 1
 fi
-umount ${BASELOC}/dev
-umount ${BASELOC}/dev/pts
-umount ${BASELOC}/dev/shm
-umount ${BASELOC}/proc
-umount ${BASELOC}/run
-umount ${BASELOC}/sys
+umount "${BASELOC}/dev/pts"
+umount "${BASELOC}/dev/shm"
+umount "${BASELOC}/dev"
+umount "${BASELOC}/proc"
+umount "${BASELOC}/run"
+umount "${BASELOC}/sys"
 
